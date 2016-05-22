f3a4cfb69df46050b113806bc4c93927f4438c3a
littletwolee <904693492@qq.com>
2016-05-22 23:53:50 +0800

    add Fragment

--- app/src/main/java/com/client/chat/hichat/MainActivity.java
+++ app/src/main/java/com/client/chat/hichat/MainActivity.java
@@ -1,16 +1,26 @@
 package com.client.chat.hichat;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements IBtnCallListener{
+    private RadioGroup rgs;
+    public List<Fragment> fragments = new ArrayList<Fragment>();
+    private HashMap<String, RadioButton> fragmentmap = new HashMap<String, RadioButton>();
+    private IBtnCallListener mBtnCallListener;
+    private int num = 0;
+    // private boolean n0, n1, n2, n3, n4, n5, n6 = false;
+    private String url, listurl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +28,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        // TODO Auto-generated method stub
+        try {
+            mBtnCallListener = (IBtnCallListener) fragment;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        super.onAttachFragment(fragment);
+    }
+    @Override
+    public void transfermsg(String v, String u) {
+        // TODO Auto-generated method stub
+//        RadioButton rb = (RadioButton) fragmentmap.get(v);
+//        if (v == "ServiceCenterToGtsPilotActivity") {
+//            ServiceCenterToGtsPilotActivity wa5 = (ServiceCenterToGtsPilotActivity) fragments
+//                    .get(6);
+//            if (u != null) {
+//                listurl = u;
+//                wa5._url = u;
+//                url = u;
+//            } else {
+//                wa5._url = listurl;
+//                url = listurl;
+//            }
+//        } else if (v == "GtsPilotInfoActivity") {
+//            GtsPilotInfoActivity wa6 = (GtsPilotInfoActivity) fragments.get(7);
+//            wa6._url = u;
+//            url = u;
+//        }
+//        rb.performClick();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -50,3 +83,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
