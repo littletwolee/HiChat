1fb9b003550eb76090906ff9a686df3f5888833e
littletwolee <904693492@qq.com>
2016-05-23 19:06:20 +0800

    add PullToRefresh

--- app/src/main/java/com/client/chat/hichat/ChatsFragment.java
+++ app/src/main/java/com/client/chat/hichat/ChatsFragment.java
@@ -1,29 +1,85 @@
 package com.client.chat.hichat;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChatsFragment extends Fragment {
-
-
-    public ChatsFragment() {
-        // Required empty public constructor
-    }
-
+    private PullToRefreshListView mPullToRefreshListView;
+    private LinkedList<String> mListItems;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        View view = inflater.inflate(R.layout.fragment_chats, container, false);
+        mPullToRefreshListView = (PullToRefreshListView)view.findViewById(R.id.chats_list);
+        mPullToRefreshListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                String label = DateUtils.formatDateTime(getContext().getApplicationContext(), System.currentTimeMillis(),
+                        DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+
+                // Update the LastUpdatedLabel
+                refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+
+                // Do work to refresh the list here.
+                new GetDataTask().execute();
+            }
+        });
+
+        ListView actualListView = mPullToRefreshListView.getRefreshableView();
+
+        mListItems = new LinkedList<String>();
+        mListItems.addAll(Arrays.asList(mStrings));
+
+        mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mListItems);
+        actualListView.setAdapter(mAdapter);
+        return view;
     }
+    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // Simulates a background job.
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+            }
+            return mStrings;
+        }
+        @Override
+        protected void onPostExecute(String[] result) {
+            mAdapter.notifyDataSetChanged();
+
+            // Call onRefreshComplete when the list has been refreshed.
+            mPullToRefreshListView.onRefreshComplete();
+            super.onPostExecute(result);
+        }
+    }
+
+    private String[] mStrings = { "John", "Michelle", "Amy", "Kim", "Mary",
+            "David", "Sunny", "James", "Maria", "Michael", "Sarah", "Robert",
+            "Lily", "William", "Jessica", "Paul", "Crystal", "Peter",
+            "Jennifer", "George", "Rachel", "Thomas", "Lisa", "Daniel", "Elizabeth",
+            "Kevin" };
 
 }
