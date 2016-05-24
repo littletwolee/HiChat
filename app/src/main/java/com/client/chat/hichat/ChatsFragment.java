c6566376e0c6e186a0c1b77b3adbb491e2acceaf
littletwolee <904693492@qq.com>
2016-05-24 18:06:47 +0800

    finished show chat list

--- app/src/main/java/com/client/chat/hichat/ChatsFragment.java
+++ app/src/main/java/com/client/chat/hichat/ChatsFragment.java
@@ -16,12 +16,15 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.client.chat.models.ChatItem;
+import com.client.chat.models.ChatItemData;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
+import java.text.Format;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -31,49 +34,44 @@ import java.util.Map;
  */
 public class ChatsFragment extends Fragment {
     private PullToRefreshListView mPullToRefreshListView;
-    private List<Map<String, Object>> mListItems;
-    private ArrayAdapter<String> mAdapter;
-
+    //private List<Map<String, Object>> mListItems;
+    private ChatAdapter mAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_chats, container, false);
-        mPullToRefreshListView = (PullToRefreshListView)view.findViewById(R.id.chats_list);
+        return inflater.inflate(R.layout.fragment_chats, container, false);
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mPullToRefreshListView = (PullToRefreshListView)getActivity().findViewById(R.id.chats_list);
+        mAdapter = new ChatAdapter(this.getContext());
         mPullToRefreshListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
             @Override
             public void onRefresh(PullToRefreshBase<ListView> refreshView) {
                 String label = DateUtils.formatDateTime(getContext().getApplicationContext(), System.currentTimeMillis(),
                         DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
-
-                // Update the LastUpdatedLabel
                 refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
-
-                // Do work to refresh the list here.
                 new GetDataTask().execute();
             }
         });
+        new GetDataTask().execute();
 
-        ListView actualListView = mPullToRefreshListView.getRefreshableView();
-
-        mListItems = new LinkedList<String>();
-        mListItems.addAll(Arrays.asList(mStrings));
-
-        mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mListItems);
-        actualListView.setAdapter(mAdapter);
-        return view;
     }
-    private class GetDataTask extends AsyncTask<Void, Void, List<ChatItem>> {
+    private class GetDataTask extends AsyncTask<Void, Void, List<ChatItemData>> {
 
         @Override
-        protected List<ChatItem> doInBackground(Void... params) {
-            // Simulates a background job.
-            List<ChatItem> chatList = null;
-            chatList = new ArrayList<ChatItem>();
-            chatList.add(new ChatItem("1", "friend", new byte[0], "lllll", "now"));
+        protected List<ChatItemData> doInBackground(Void... params) {
+            List<ChatItemData> chatList = new ArrayList<ChatItemData>();
+            for (int i = 1; i <= 20; i++) {
+                chatList.add(new ChatItemData(String.valueOf(i), "line"+String.valueOf(i), "friend", null, "hahahah", (new Date()).toString()));
+            }
             return chatList;
         }
         @Override
-        protected void onPostExecute(List<ChatItem> result) {
+        protected void onPostExecute(List<ChatItemData> result) {
+            mAdapter.data = result;
+            mPullToRefreshListView.getRefreshableView().setAdapter(mAdapter);
             mAdapter.notifyDataSetChanged();
 
             // Call onRefreshComplete when the list has been refreshed.
@@ -85,6 +83,7 @@ public class ChatsFragment extends Fragment {
     public class ChatAdapter extends BaseAdapter
     {
         private LayoutInflater mInflater = null;
+        public  List<ChatItemData> data;
         private ChatAdapter(Context context)
         {
             //根据context上下文加载布局，这里的是Demo17Activity本身，即this
@@ -94,7 +93,7 @@ public class ChatsFragment extends Fragment {
         public int getCount() {
             //How many items are in the data set represented by this Adapter.
             //在此适配器中所代表的数据集中的条目数
-            return mListItems.size();
+            return data.size();
         }
         @Override
         public Object getItem(int position) {
@@ -120,19 +119,22 @@ public class ChatsFragment extends Fragment {
                 chatItem = new ChatItem();
                 //根据自定义的Item布局加载布局
                 convertView = mInflater.inflate(R.layout.chat_list_item, null);
-                chatItem.Pic = (ImageView)convertView.findViewById(R.id.img);
-                chatItem.title = (TextView)convertView.findViewById(R.id.tv);
-                chatItem.info = (TextView)convertView.findViewById(R.id.info);
+                chatItem.ChatID = data.get(position).ChatID;
+                chatItem.ChatName = (TextView)convertView.findViewById(R.id.chat_name);
+                chatItem.ChatType = data.get(position).ChatType;
+                chatItem.Pic = (ImageView)convertView.findViewById(R.id.pic);
+                chatItem.LastMsg = (TextView)convertView.findViewById(R.id.last_msg);
+                //chatItem.setLastTime(data.get(position).LastTime);
                 //将设置好的布局保存到缓存中，并将其设置在Tag里，以便后面方便取出Tag
                 convertView.setTag(chatItem);
             }else
             {
                 chatItem = (ChatItem)convertView.getTag();
             }
-            chatItem.img.setBackgroundResource((Integer) mListItems.get(position).get("img"));
-            chatItem.title.setText((String) mListItems.get(position).get("title"));
-            chatItem.info.setText((String) mListItems.get(position).get("info"));
-
+            //chatItem.Pic.setBackgroundResource();
+            chatItem.ChatName.setText((String) data.get(position).ChatName);
+            chatItem.LastMsg.setText((String) data.get(position).LastMsg);
+            chatItem.Pic.setImageResource(R.mipmap.ic_launcher);
             return convertView;
         }
 
--- app/src/main/java/com/client/chat/models/ChatItem.java
+++ app/src/main/java/com/client/chat/models/ChatItem.java
@@ -1,5 +1,11 @@
 package com.client.chat.models;
 
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Date;
+
 /**
  * Created by lee on 16-5-23.
  */
@@ -8,16 +14,25 @@ public class ChatItem {
     public  ChatItem(){}
     public ChatItem(String chatID, String chatName, String chatType, byte[] pic, String lastMsg, String lastTime) {
         this.ChatID = chatID;
-        this.ChatName = chatName;
+        this.ChatName.setText(chatName);
         this.ChatType = chatType;
-        this.Pic = pic;
-        this.LastMsg = lastMsg;
-        this.LastTime = lastTime;
+        this.setPic(pic);
+        this.LastMsg.setText(lastMsg);
+        this.LastTime.setText(lastTime);
     }
     public String ChatID;
-    public String ChatName;
+    public TextView ChatName;
     public String ChatType;
-    public byte[] Pic;
-    public String LastMsg;
-    public String LastTime;
+    public ImageView Pic;
+    public void setPic(byte[] pic) {
+//        if (pic != null) {
+//            this.Pic.setImageBitmap(BitmapFactory.decodeByteArray(pic, 0, pic.length));
+//        }
+        this.Pic.setImageResource(android.R.mipmap.sym_def_app_icon);
+    }
+    public TextView LastMsg;
+    public TextView LastTime;
+    public void setLastTime(String lastTime) {
+        this.LastTime.setText(lastTime);
+    }
 }
