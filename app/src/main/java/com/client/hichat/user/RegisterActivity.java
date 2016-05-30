f2384fca4fca36b231992985c28854af3b57259f
littletwolee <904693492@qq.com>
2016-05-30 15:30:35 +0800

    modify login & register ui

--- app/src/main/java/com/client/hichat/user/RegisterActivity.java
+++ app/src/main/java/com/client/hichat/user/RegisterActivity.java
@@ -2,11 +2,12 @@ package com.client.hichat.user;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.client.hichat.R;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private Button btn_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
