94bdb0c9cb2b23dfa91668a7364762c8b2e2cdca
littletwolee <904693492@qq.com>
2016-06-02 18:00:15 +0800

    add greendao & freemarker

---
+++ app/src/main/java/com/client/daogenerater/modelgenerater.java
@@ -0,0 +1,26 @@
+package com.client.daogenerater;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+/**
+ * Created by lee on 6/2/16.
+ */
+public class modelgenerater {
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1000, "com.client.models");
+
+        addNote(schema);
+
+        new DaoGenerator().generateAll(schema, "./app/src/main/java");
+    }
+
+    private static void addNote(Schema schema) {
+        Entity user = schema.addEntity("User");
+        user.addIdProperty();
+        user.addStringProperty("username").notNull();
+        user.addStringProperty("password").notNull();
+        user.addDateProperty("lastlogin").notNull();
+    }
+}
